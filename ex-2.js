73b28d1950ceec7b5817fa7616167ca32bf42236
James <apisitamornktn@gmail.com>
2024-07-17 15:09:31 +0700

    feat:binary search

--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,30 @@
 function findBookIndex(books, searchTitle) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  let left = 0;
+  let right = books.length - 1;
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+    if (books[middle].title === searchTitle) {
+      return middle;
+    } else if (books[middle].title < searchTitle) {
+      left = middle + 1;
+    } else {
+      right = middle - 1;
+    }
+  }
+  return -1;
 }
+
+const books = [
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" },
+];
+
+console.log(findBookIndex(books, "Gone with the Wind"));
